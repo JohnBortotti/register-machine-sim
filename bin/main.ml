cf9bca9c3636046f5eaa60d7cb50dcd0212af19f
JoaoBortotti <joao.bortotti@uotz.com.br>
2022-12-08 14:49:12 -0300

    Implemeting stack module and instructions operands
    Add stack module operations for handle stacks
    Create type memory_instruction to handle instruction operands in memory

--- bin/main.ml
+++ bin/main.ml
@@ -1,28 +1,57 @@
 open Register_machine_sim
 
-(* [] create stack operations *)
-(* [] add operands evaluation *)
-let first_machine = create_machine
-    0
+let first_machine = Machine.create_machine
     [|{value=0 }; {value=0}|]
-    {s=[]}
-    [|"rprt 0"; "radd 0 1"; "rprt 0"|]
+    [|
+        {opcode="rprt"; operands=[|1|]}; 
+        {opcode="radd"; operands=[|1|]}; 
+        {opcode="rprt"; operands=[|1|]};
+        {opcode="stckp"; operands=[|3|]};
+        {opcode="stckp"; operands=[|6|]};
+        {opcode="stckp"; operands=[|2|]};
+        {opcode="stckd"; operands=[||]};
+        {opcode="stckd"; operands=[||]};
+    |]
 
-let instructions_dictionary = [
-        {opcode="rprt"; func=(
-            function machine -> 
-                machine.pc <- machine.pc+1; 
-                Printf.printf "%d \n" machine.registers.(0).value;
-                machine 
-        )};
-        {opcode="radd"; func=(
-            function machine -> 
-                machine.pc <- machine.pc+1; 
-                machine.registers.(0).value <- machine.registers.(0).value + 1;
-                machine 
-        )}
-    ]
+let instructions_dictionary: Machine.instruction_signature list = [
+        {
+            opcode="rprt";
+            func=(
+                function (machine, operands) -> 
+                    machine.pc <- machine.pc+1; 
+                    Printf.printf "%d \n" machine.registers.(operands.(0)).value;
+                    machine 
+            )
+        };
+        {
+            opcode="radd"; 
+            func=(
+                function (machine, operands) -> 
+                    machine.pc <- machine.pc+1; 
+                    machine.registers.(operands.(0)).value <- machine.registers.(operands.(0)).value + 1;
+                    machine 
+            )
+        };
+        {
+            opcode="stckp"; 
+            func=(
+                function (machine, operands) -> 
+                    machine.pc <- machine.pc+1;
+                    Stack.push operands.(0) machine.stack;
+                    machine
+            )
+            
+        };
+        {
+            opcode="stckd"; 
+            func=(
+                function (machine, _) -> 
+                    machine.pc <- machine.pc+1;
+                    Printf.printf "removed from stack: %d \n" (Stack.pop machine.stack);
+                    machine
+            )
+            
+        };
+]
     
-let () =
-    let final_machine = execute_instructions first_machine instructions_dictionary in
-    Printf.printf "%d" final_machine.pc
+let () = Machine.execute_instructions first_machine instructions_dictionary
--- lib/register_machine_sim.ml
+++ lib/register_machine_sim.ml
@@ -1,21 +1,38 @@
-type register = { mutable value: int }
-type stack = { mutable s : int list }
+module Stack = struct
+    type t = { mutable s : int list }
 
-type instruction = { opcode: string; func: machine -> machine }
-and machine = { mutable pc: int; mutable registers: register array ; mutable stack: stack; memory: string array;}
+    exception EmptyStack
 
-exception InstructionNotFound of string
-exception RegisterNotFound of string
+    let create() = {s = []}
+
+    let pop s = 
+        match s.s with
+        | [] -> raise EmptyStack
+        | hd::tl -> s.s <- tl; hd
+
+    let push x s = s.s <- x :: s.s 
+end
+
+module Machine = struct
+    type register = {mutable value: int}
+    type operands = int array
+    type memory_instruction = {opcode: string; operands: operands}
+    type instruction_signature = {opcode: string; func: (machine * operands) -> machine}
+    and machine = {mutable pc: int; mutable registers: register array ; mutable stack: Stack.t; memory: memory_instruction array;}
+
+    exception InstructionNotFound of string
     
-let create_machine pc registers stack memory = {pc; registers; stack; memory}
+    let create_machine registers memory = {pc=0; registers; stack=(Stack.create)(); memory}
 
-let rec decode_instruction (opcode: string) (instructions: instruction list) = 
-    let sub_opcode = String.sub opcode 0 4  in
-    match instructions with
-    | [] -> raise (InstructionNotFound opcode)
-    | hd :: tl -> if hd.opcode = sub_opcode then hd else decode_instruction opcode tl
+    let rec decode_instruction (memory_instruction: memory_instruction) (instructions: instruction_signature list) =
+        let opcode =  memory_instruction.opcode in
+        match instructions with
+        | [] -> raise (InstructionNotFound opcode)
+        | hd :: tl -> if hd.opcode = opcode then (hd, memory_instruction.operands) else decode_instruction memory_instruction tl
+        
 
-let rec execute_instructions (machine) (instructions: instruction list) =
-    let instruction = decode_instruction machine.memory.(machine.pc) instructions in
-    Printf.printf "executing instruction: %s \n" instruction.opcode;
-    execute_instructions (instruction.func machine) instructions
+    let rec execute_instructions (machine) (instructions: instruction_signature list) =
+        let instruction , operands = decode_instruction machine.memory.(machine.pc) instructions in
+        Printf.printf "executing instruction: %s \n" instruction.opcode;
+        execute_instructions (instruction.func (machine, operands)) instructions
+end
